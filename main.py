ad739427f80547d662d79778370be6ee46f74e63
JoeH2025 <joseph.robert.huber@gmail.com>
2025-11-19 15:50:52 -0600

    main call followers

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from scrape_followers import get_most_followed
-github_username = "NAME_HERE"
+github_username = "Joe-Huber"
 github_followers_link = f"https://github.com/{github_username}?tab=followers"
+
+if __name__ == "__main__":
+    most_followed = get_most_followed(github_followers_link, 2)
+    print(most_followed)
