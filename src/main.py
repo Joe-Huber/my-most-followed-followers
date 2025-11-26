b8464e4d52b2237c8fdba97292f25837d73fe84c
JoeH2025 <joseph.robert.huber@gmail.com>
2025-11-25 18:20:58 -0800

    unnesecary src

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from src.scrape_followers import get_most_followed
+from scrape_followers import get_most_followed
 github_username = "Joe-Huber"
 github_followers_link = f"https://github.com/{github_username}?tab=followers"
 top_number = 5
